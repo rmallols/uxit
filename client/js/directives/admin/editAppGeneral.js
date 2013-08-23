db1fc84a7a8e374aed5db3763d8e9b168d4d4623
rmallols <ricardo.mallols@gmail.com>
2013-08-23 18:34:54 +0200

    #91 Align Apps content to the center

--- client/js/directives/admin/editAppGeneral.js
+++ client/js/directives/admin/editAppGeneral.js
@@ -9,12 +9,13 @@ COMPONENTS.directive('editAppGeneral', ['i18nService', function (i18nService) {
             onLayerSave : '='
         },
         link: function link(scope) {
-            scope.floats = [
-                { id: 'left',  text: i18nService('editApp.floatLeft') },
-                { id: 'right', text: i18nService('editApp.floatRight') }
+            scope.aligns = [
+                { id: 'left',   text: i18nService('editApp.alignLeft') },
+                { id: 'center', text: i18nService('editApp.alignCenter') },
+                { id: 'right',  text: i18nService('editApp.alignRight') }
             ];
-            if(!scope.model.float) {
-                scope.model.float = scope.floats[0].id;
+            if(!scope.model.align) {
+                scope.model.align = scope.aligns[0].id;
             }
         }
 	};
