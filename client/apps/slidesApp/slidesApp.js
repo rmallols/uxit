0f54c5c18d99301519d4806d0547ed321690a98f
rmallols <ricardo.mallols@gmail.com>
2013-08-26 17:34:28 +0200

    #79 Installation of Reveal.js in the context of a new APP

--- client/apps/slidesApp/slidesApp.js
+++ client/apps/slidesApp/slidesApp.js
@@ -9,44 +9,46 @@ COMPONENTS.directive('slidesAppView', [function () {
             internalData: '=',
             onLayerSave: '='
         },
-        link: function link() {
+        link: function link(scope) {
 
             var appPath = '/client/apps/slidesApp/';
+
+            scope.model.slides = [
+                { content: '<h1>Hello slides app!</h1><h3>Welcome to the fancy app to show presentations</h3>(In progress...)' },
+                { content: '<h3>Discramiler</h3>This is a harcoded example about how to add cool stuff with small effort' }
+            ];
 
             yepnope({
                 load: [
                     appPath + 'css/reveal.min.css',
-                    appPath + 'css/theme/default.css',
                     appPath + 'js/reveal.js',
                     appPath + 'lib/js/head.min.js'
                 ],
                 complete: function () {
-                    // Full list of configuration options available here:
-                    // https://github.com/hakimel/reveal.js#configuration
-                    setTimeout(function() {
-                        console.log("RV2", Reveal.getQueryHash())
-                        Reveal.initialize({
-                            controls: true,
-                            progress: true,
-                            history: true,
-                            center: true,
-
-                            theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-                            transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
-
-                            // Optional libraries used to extend on reveal.js
-                            dependencies: [
-                                { src: appPath + 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-                                { src: appPath + 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-                                { src: appPath + 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-                                { src: appPath + 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-                                { src: appPath + 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-                                { src: appPath + 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
-                            ]
-                        });
-                    }, 1000)
+                    initializeReveal();
                 }
             });
+
+            /** Private methods **/
+            function initializeReveal() {
+                Reveal.initialize({
+                    controls: true,
+                    progress: true,
+                    history: false,
+                    center: true,
+                    theme: Reveal.getQueryHash().theme,
+                    transition: Reveal.getQueryHash().transition || 'default',
+                    dependencies: [ // Optional libraries used to extend on reveal.js
+                        { src: appPath + 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+                        { src: appPath + 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+                        { src: appPath + 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+                        { src: appPath + 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+                    ]
+                });
+                scope.show = true;
+                scope.$apply();
+            }
+            /** End of private methods **/
         }
 	};
 }]);
