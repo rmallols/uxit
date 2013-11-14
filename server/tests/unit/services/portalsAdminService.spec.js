c4d0071364735ab4bd6ef7a5d46cb3fba4e04f68
rmallols <ricardo.mallols@gmail.com>
2013-11-14 08:46:29 +0100

    #163 Generalize the app-bridge mechanism to make it compatible with core resources

--- server/tests/unit/services/portalsAdminService.spec.js
+++ server/tests/unit/services/portalsAdminService.spec.js
@@ -5,34 +5,67 @@ describe('dbService', function () {
     beforeEach(module("components"));
     beforeEach(inject(["$rootScope", "$httpBackend", "portalsAdminService", "dbService",
     function ($rootScope,$httpBackend,  pAS_, dbS) {
-        $scope = $rootScope.$new();
-        pAS = pAS_;
+        $scope          = $rootScope.$new();
+        $scope.model    = { name: 'testPortal' };
+        pAS             = pAS_;
         loadDatabases($httpBackend, dbS, null);
-        pAS.view($scope);
     }]));
 
     describe('view', function() {
 
-        iit('should set the proper config object', function() {
+        beforeEach(function() {
+            pAS.view($scope);
+        });
+
+        it('should set the proper config object', function() {
             expect($scope.config.multiSelectable).toBe(true);
             expect($scope.config.creatable).toBe(true);
             expect($scope.config.editable).toBe(true);
             expect($scope.config.deletable).toBe(true);
         });
 
-        iit('should set the proper template', function() {
+        it('should set the proper template', function() {
             expect($($scope.template).hasClass('columns large-25')).toBe(true);
         });
 
-        iit('should set the proper edit panel length', function() {
-            expect($scope.onEditPanels.length).toBe(1);
+        describe('edit panel', function() {
+
+            it('should set the proper edit panel length', function() {
+                expect($scope.onEditPanels.length).toBe(1);
+            });
+
+            it('should set the proper edit panel data', function() {
+                expect($scope.onEditPanels[0].type).toBe('editDb');
+                expect($scope.onEditPanels[0].view).toBe('editDb');
+                expect($scope.onEditPanels[0].src).toBe('portalsAdmin');
+                expect($scope.onEditPanels[0].appBridge).toBe(true);
+            });
         });
 
-        iit('should set the proper edit panel data', function() {
-            expect($scope.onEditPanels[0].type).toBe('editDb');
-            expect($scope.onEditPanels[0].view).toBe('editDb');
-            expect($scope.onEditPanels[0].src).toBe('portalsAdmin');
-            expect($scope.onEditPanels[0].appBridge).toBe(true);
+        describe('create panel', function() {
+
+            it('should set the proper create panel length', function() {
+                expect($scope.onCreatePanels.length).toBe(1);
+            });
+
+            it('should set the proper edit panel data', function() {
+                expect($scope.onCreatePanels[0].type).toBe('createDb');
+                expect($scope.onCreatePanels[0].view).toBe('createDb');
+                expect($scope.onCreatePanels[0].src).toBe('portalsAdmin');
+                expect($scope.onCreatePanels[0].appBridge).toBe(true);
+            });
+        });
+    });
+
+    describe('editDb', function() {
+
+        beforeEach(function() {
+            pAS.editDb($scope);
+        });
+
+        it('should initialize the typedName variable', function() {
+            expect($scope.model.typedName).not.toBeUndefined();
+            expect($scope.model.typedName).toBe($scope.model.name);
         });
     });
 });
